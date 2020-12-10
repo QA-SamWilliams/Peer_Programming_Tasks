f829d6cafc4e35addc00dc13eb0119ed093d980d
qa-ihussain <ihussain@qa.com>
2020-12-10 11:55:02 +0000

    changes

--- com/qa/doggo/Runner.java
+++ com/qa/doggo/Runner.java
@@ -1,7 +1,7 @@
 package com.qa.doggo;
 
 public class Runner {
-    public static mains(String [] args) {
+    public static void mains(String[] args) {
         int i = 1;
         while (i <= 100) {
             System.out.println(i);
